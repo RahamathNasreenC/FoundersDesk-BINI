73f10d8f1ab496bff607a7b00657e14c879393cc
RahamathNasreenC <rahamathnasreen401@gmail.com>
2026-02-04 19:01:37 +0530

    migration refining

---
+++ Migrations/20260131132942_RecreateResourceAndDigital.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoundersDesk.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecreateResourceAndDigital : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20260131133321_RecreateResourceAckAndDigitalSign.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+#nullable disable
+
+namespace FoundersDesk.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecreateResourceAckAndDigitalSign : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+    name: "ResourceAcknowledgements",
+    columns: table => new
+    {
+        Id = table.Column<int>(nullable: false)
+            .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+
+        DocumentId = table.Column<int>(nullable: false),
+
+        Username = table.Column<string>(nullable: true),
+
+        AcknowledgedAt = table.Column<DateTime>(nullable: false)
+    },
+    constraints: table =>
+    {
+        table.PrimaryKey("PK_ResourceAcknowledgements", x => x.Id);
+    });
+
+            migrationBuilder.CreateTable(
+                name: "DigitalSignatures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+
+                    Username = table.Column<string>(nullable: true),
+
+                    FilePath = table.Column<string>(nullable: true),
+
+                    UploadedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DigitalSignatures", x => x.Id);
+                });
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
